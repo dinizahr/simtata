cbef1c89cb452f4b3e9715102fa6dbc65ea739f4
dinizahr <108143463+dinizahr@users.noreply.github.com>
2023-02-27 16:40:17 +0700

    profil
    - menambah variabel kelas didalam profil

--- content/profil.php
+++ content/profil.php
@@ -18,6 +18,7 @@
                     <tr>
                         <center><th><center>No</center></th></center>
                         <center><th><center>Nama</center></th></center>
+                        <center><th><center>Kelas</center></th></center>
                         <center><th><center>TTL</center></th></center>
                         <center><th><center>Alamat</center></th></center>
                     </tr>
@@ -26,38 +27,44 @@
                     <tr>
                             <td> 1</td>
                             <td>Ahmad Syaifuddin Nur Wahid</td>
+                            <td>XI RPL</td>
                            <td>Madiun,17-02-2006</td>
                            <td>Gadang,Pucanganom,Kebonsari,Madiun</td>
                     </tr>
                     <tr>
                             <td> 2</td>
                             <td>Uhailul Munawwar</td>
+                            <td>XI RPL</td>
                             <td>Madiun,12-06-2006</td>
                             <td>Melikan,Purwosari,Babadan,Ponorogo</td>
                     </tr>
                     <tr>
                             <td> 3</td>
                             <td>Abid Ainur Rofi'</td>
+                            <td>XI RPL</td>
                             <td>Madiun,03-06-2006</td>
                             <td>Bangunsari,Dolopo,Dolopo,Madiun</td>
                     </tr>
                     <tr>
-                       <td> 4</td>
-                       <td>Dini Zahrotul 'Azizah</td>
-                        <td>Madiun,22-03-2006</td>
-                        <td> Klagenserut,Jiwan,Madiun</td>
+                            <td> 4</td>
+                            <td>Dini Zahrotul 'Azizah</td>
+                            <td>XI RPL</td>
+                            <td>Madiun,22-03-2006</td>
+                            <td> Klagenserut,Jiwan,Madiun</td>
                     </tr>
                     <tr>
-                        <td> 5</td>
-                        <td>Siti Multazimatussholikhah</td>
-                        <td>Madiun,30-10-2005</td>
-                        <td>Tanjungrejo,Kebonsari,Madiun</td>
+                            <td> 5</td>
+                            <td>Siti Multazimatussholikhah</td>
+                            <td>XI RPL</td>
+                            <td>Madiun,30-10-2005</td>
+                            <td>Tanjungrejo,Kebonsari,Madiun</td>
                     </tr>
                     <tr>
-                        <td> 6</td>
-                        <td>Adelin Azizatul Nabfiqah</td>
-                        <td>Madiun,25-08-2005</td>
-                        <td>Krandegan,Kebonsari,Madiun</td>
+                            <td> 6</td>
+                            <td>Adelin Azizatul Nabfiqah</td>
+                            <td>XI RPL</td>
+                            <td>Madiun,25-08-2005</td>
+                            <td>Krandegan,Kebonsari,Madiun</td>
                     </tr>
                     </tbody>
                 </table><br><hr>
